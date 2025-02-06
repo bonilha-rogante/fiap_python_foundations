39502104f78aad333634e2d8b49aa7cb51e820c3
Bonilha <bonilha.rogante@gmail.com>
2025-02-05 21:59:47 -0300

    Ajustando as branches

--- exercicios/area_triangulo.py
+++
@@ -1,9 +0,0 @@
-#1) Entrar via teclado com a base e a altura de um retângulo, calcular e exibir sua área.
-# Fórmula: base * altura
-
-base = float(input('Digite a base do triângulo:'))
-altura = float(input('Digite a altura do triângulo: '))
-
-area_triangulo = base * altura / 2
-
-print(f'A área do triângulo é {area_triangulo:.2f}')
