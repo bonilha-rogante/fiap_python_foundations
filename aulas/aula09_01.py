78873fc198ec0eda970dbe175e615d2aa09dbb86
Bonilha <bonilha.rogante@gmail.com>
2025-02-12 10:14:52 -0300

    Aula 09 |def __repr__

--- aulas/aula09_01.py
+++ aulas/aula09_01.py
@@ -2,10 +2,10 @@ class Aluno:
     def __init__(self, nome):
         self.nome = nome
         
-    def __str__(self): #
+    def __str__(self): #fromato que vc deseja que o usário visualize seu objeto
         return f'Nome: {self.nome}'
     
-    def __repr__(self): # Exibido em Debugger e arquivos de log | 
+    def __repr__(self): # Exibido em Debugger e arquivos de log | Formato que você deseja visualizar o objeto
         return f'{self.__class__.__name__}("{self.nome}")"'
 
 print(f'__name__ do módulo {__name__}')
